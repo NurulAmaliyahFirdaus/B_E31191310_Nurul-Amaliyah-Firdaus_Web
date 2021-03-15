155695740000512ee8b52233412471cab7292c7b
NurulAmaliyahFirdaus <66766615+NurulAmaliyahFirdaus@users.noreply.github.com>
2021-03-14 21:56:18 -0700

    Update Tugas3_minggu6.php

--- Minggu_6/CodeIgniter/application/views/Tugas3_minggu6.php
+++ Minggu_6/CodeIgniter/application/views/Tugas3_minggu6.php
@@ -74,10 +74,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<p>Tugas 3 Minggu 6</p>
 
 		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>Nurul Amaliyah Firdaus E31191310 B</code>
+		<code>NURUL AMALIYAH FIRDAUS E31191310 B</code>
 
 		<p>The corresponding controller for this page is found at:</p>
-		<code>Nurul Amaliyah Firdaus E31191310 B</code>
+		<code>NURUL AMALIYAH FIRDAUS E31191310 B</code>
 
 		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
 	</div>
