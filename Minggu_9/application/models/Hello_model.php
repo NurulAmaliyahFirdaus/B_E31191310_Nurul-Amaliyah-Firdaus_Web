22df29f506c4c6b7e5fddfd7c20d00ab2f6ed064
NurulAmaliyahFirdaus <66766615+NurulAmaliyahFirdaus@users.noreply.github.com>
2021-04-06 19:44:13 -0700

    Update Hello_model.php

--- Minggu_9/application/models/Hello_model.php
+++ Minggu_9/application/models/Hello_model.php
@@ -5,8 +5,8 @@ class Hello_model extends CI_Model{
     public $alert = 'Hello Wolrd dari CI Model';
     
     public $mahasiswa = [
-			["nama"=>"Putra","nim"=>"E31191250","gol"=>"Internasional"],
-			["nama"=>"Mega","nim"=>"E31191394","gol"=>"Internasional"],
-			["nama"=>"Inan","nim"=>"E31191124","gol"=>"Internasional"] 
+			["nama"=>"Firda","nim"=>"E3119110","gol"=>"B"],
+			["nama"=>"Vival","nim"=>"E31190875","gol"=>"B"],
+			["nama"=>"Shindi","nim"=>"E31191120","gol"=>"B"] 
 		];
 }
