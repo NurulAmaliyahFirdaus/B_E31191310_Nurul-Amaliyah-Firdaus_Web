b2f175d6c9a2bf8fa1e757a498230a8dabe55587
NurulAmaliyahFirdaus <66766615+NurulAmaliyahFirdaus@users.noreply.github.com>
2021-04-14 22:06:57 -0700

    E31191310_Nurul Amaliyah Firdaus_B

---
+++ Minggu_11_Tugas_CRUD_1/application/models/Mahasiswa_model.php
@@ -0,0 +1,29 @@
+<?php 
+// membuat class model, dengan syarat nama file sama dengan nama Class
+class Mahasiswa_model extends CI_Model {
+  // idenfikasi variable
+  public function get_data(){ //memanggil fungsi get data
+    $data_mahasiswa = [ //data dari mahasiswa
+			["nama"=>"Nurul Amaliyah Firdaus","prodi"=>"MIF"],
+			["nama"=>"Ahcmad Bagus Pratama","prodi"=>"TKK"],
+			["nama"=>"Sinta Nuriyah Wulandari","prodi"=>"TIF"]
+		];
+		// mengembalikan nilai variabel data angkatan
+    return $data_mahasiswa;
+  }
+
+  public function getAll(){ //method untuk mengambil data dari database
+    $this->db->select('*'); 
+    $this->db->from('tm_user');
+    $this->db->join('tm_grup','tm_user.grup = tm_grup.id_grup');
+    $query = $this->db->get();
+    // mengembalikan nilai variabel query
+    return $query;
+  }
+
+  public function input_data($data,$table) //menanggil fungsi input data
+  {
+    $this->db->insert($table,$data); //berfungsi untuk menyimpan ke database
+  }
+}
+?>
