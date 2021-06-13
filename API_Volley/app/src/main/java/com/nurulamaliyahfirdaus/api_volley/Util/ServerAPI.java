44b2b14f235020657316117d417af8dcc706e19b
NurulAmaliyahFirdaus <66766615+NurulAmaliyahFirdaus@users.noreply.github.com>
2021-06-13 09:28:49 -0700

    E31191310_NurulAmaliyahFirdaus_API_Volley

---
+++ API_Volley/app/src/main/java/com/nurulamaliyahfirdaus/api_volley/Util/ServerAPI.java
@@ -0,0 +1,8 @@
+package com.nurulamaliyahfirdaus.api_volley.Util;
+
+public class ServerAPI {
+    public static final String URL_DATA = "http://192.168.43.164/TugasWebFramework/B_E31191310_Nurul-Amaliyah-Firdaus_Web/Rest_API/Mahasiswa/Api";
+    public static final String URL_INSERT = "http://192.168.43.164/TugasWebFramework/B_E31191310_Nurul-Amaliyah-Firdaus_Web/Rest_API/Mahasiswa/ApiInsert";
+    public static final String URL_DELETE = "http://192.168.43.164/TugasWebFramework/B_E31191310_Nurul-Amaliyah-Firdaus_Web/Rest_API/Mahasiswa/ApiDelete";
+    public static final String URL_UPDATE = "http://192.168.43.164/TugasWebFramework/B_E31191310_Nurul-Amaliyah-Firdaus_Web/Rest_API/Mahasiswa/ApiUpdate";
+}
